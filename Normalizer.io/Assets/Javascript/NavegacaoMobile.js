e3a69bafd31318264ccb78d92b760b013ff289f5
Marcus-Barbosa752 <marcusbarbosa175@gmail.com>
2026-02-03 20:52:12 -0300

    Delete Normalizer.io directory

--- Normalizer.io/Assets/Javascript/NavegacaoMobile.js
+++
@@ -1,9 +0,0 @@
-const BtnToggleMenuFloatMobile = document.getElementById("BtnToggleMenuFloatMobile")
-
-const SpanUmToggleMenuFloatMobile = document.getElementById("SpanUmToggleMenuFloatMobile")
-const SpanDoisToggleMenuFloatMobile = document.getElementById("SpanDoisToggleMenuFloatMobile")
-
-BtnToggleMenuFloatMobile.onclick = () => {
-    SpanUmToggleMenuFloatMobile.classList.toggle("ActiveSpanUmToggleMenuFloatMobile")
-    SpanDoisToggleMenuFloatMobile.classList.toggle("ActiveSpanDoisToggleMenuFloatMobile")
-}
