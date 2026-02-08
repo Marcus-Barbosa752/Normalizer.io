574aaca794f871523356237442caa66c16aa2f74
Marcus-Barbosa752 <marcusbarbosa175@gmail.com>
2026-02-08 12:51:15 -0300

    Update NavegacaoMobile.js

--- Assets/Javascript/NavegacaoMobile.js
+++ Assets/Javascript/NavegacaoMobile.js
@@ -1,9 +1,35 @@
+import { Exemplo_Chat } from "./Exemplo.js"
+
 const BtnToggleMenuFloatMobile = document.getElementById("BtnToggleMenuFloatMobile")
+const BtnCloseMenuMobile = document.getElementById("BtnCloseMenuMobile")
+const BtnCleanChatMobile = document.getElementById("BtnCleanChatMobile")
 
 const SpanUmToggleMenuFloatMobile = document.getElementById("SpanUmToggleMenuFloatMobile")
 const SpanDoisToggleMenuFloatMobile = document.getElementById("SpanDoisToggleMenuFloatMobile")
 
+const MenuFloatMobile = document.getElementById("MenuFloatMobile")
+
 BtnToggleMenuFloatMobile.onclick = () => {
-    SpanUmToggleMenuFloatMobile.classList.toggle("ActiveSpanUmToggleMenuFloatMobile")
-    SpanDoisToggleMenuFloatMobile.classList.toggle("ActiveSpanDoisToggleMenuFloatMobile")
+    SpanUmToggleMenuFloatMobile.classList.add("ActiveSpanUmToggleMenuFloatMobile")
+    SpanDoisToggleMenuFloatMobile.classList.add("ActiveSpanDoisToggleMenuFloatMobile")
+    MenuFloatMobile.classList.add("AddMenuFloatMobile")
+}
+
+BtnCloseMenuMobile.onclick = () => {
+    SpanUmToggleMenuFloatMobile.classList.remove("ActiveSpanUmToggleMenuFloatMobile")
+    SpanDoisToggleMenuFloatMobile.classList.remove("ActiveSpanDoisToggleMenuFloatMobile")
+    MenuFloatMobile.classList.remove("AddMenuFloatMobile")
+}
+
+BtnCleanChatMobile.onclick = () => {
+    InsertValue.innerHTML = `
+    <h4 id="H4TitleChat">Mesmo bagunçado, seu nome sai do jeito certo.</h4>
+    <h3 id="H3SubTitleChat">Normalização inteligente de nomes..</h3>
+    <h4 id="H4MensagemChat">Pode bagunçar. Eu arrumo.</h4>
+    <button id="BtnTextoExemploChat">Exemplo</button>
+    `
+
+    document.getElementById("BtnTextoExemploChat").onclick = () => Exemplo_Chat()
+    BtnCleanChat.style.display = "none"
+    BtnCloseMenuMobile.click()
 }
